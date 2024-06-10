78984a2c112d87c4fa25c09d666f291b540e0bf6
Alex Calero Revilla <alexcalerorevilla@gmail.com>
2024-06-10 18:24:25 -0500

    Desarrollo del inciso B de la pregunta 2 del laboratorio 1 del ciclo 2023-2

---
+++ LAB1/2023-2/LAB1-2023_2-P2-B/Antivirus.h
@@ -0,0 +1,18 @@
+/*
+ * Proyecto : LAB1-2023_2-P2-B
+ * Archivo  : Antivirus.h
+ * Autor    : Alex Calero Revilla
+ * Codigo   : 20206455
+ * Fecha    : 31 de marzo de 2024, 21:11
+ */
+
+#ifndef ANTIVIRUS_H
+#define ANTIVIRUS_H
+
+struct Antivirus {
+	int inversion;
+	int beneficio;
+	int requisitos[3];
+};
+
+#endif /* ANTIVIRUS_H */
